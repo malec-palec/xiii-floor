218f68ddd9b6dfcfb9f9ee7c5acb5c44b4e7b1dc
glebv <glebusheg@gmail.com>
2024-09-01 22:51:18 +0200

    add zip pipeline

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+import { exec } from "child_process";
+import fs from "fs-extra";
+import gulp from "gulp";
+import size from "gulp-size";
+import zip from "gulp-zip";
+
+const zipFile = "game.zip";
+
+const clearOut = () => fs.emptyDir("./out");
+
+const zipDist = () =>
+  gulp
+    .src(["./dist/*", "!./dist/*.json"])
+    .pipe(size({ showFiles: true }))
+    .pipe(zip(zipFile))
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest("./out"));
+
+const shrink = (cb) => {
+  exec(`cd tools && advzip ../out/${zipFile} -z -4 -i 1000`, function (err, stdout, stderr) {
+    console.log(stdout);
+    cb(err);
+  });
+};
+
+const reportSize = (cb) => {
+  const stats = fs.statSync(`out/${zipFile}`);
+  const sizeKb = stats.size / 1024;
+  const ratio = (stats.size / (13 * 1024)) * 100;
+  console.log(`Size:	${sizeKb.toFixed(2)} KB
+Ratio:	${ratio.toFixed(2)} %`);
+  cb();
+};
+
+export default gulp.series(clearOut, zipDist, shrink, reportSize);
