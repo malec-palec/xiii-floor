fbed2078c6567481c9d1513f982e74ccdc973025
glebv <glebusheg@gmail.com>
2024-09-08 15:23:35 +0200

    scene dimensions scene dimensions

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,16 +1,46 @@
 import { IGame } from "../game";
-import { isMobile } from "../registry";
+import { COLOR_WHITE, isMobile } from "../registry";
 import { logDebug } from "../utils";
 import { BaseScene } from "./scene";
 
+type GameSceneDimensions = {
+  floorHeight: number;
+  wallSize: number;
+  gameAreaSize: number;
+  canvasSize: number;
+  sidebarSize: number;
+  sceneWidth: number;
+  sceneHeight: number;
+};
+
+export const getGameSceneDimensions = (numFloors: number): GameSceneDimensions => {
+  const floorHeight = 16 * 4;
+  const wallSize = 16 / 2;
+  const gameAreaSize = numFloors * floorHeight;
+  const canvasSize = gameAreaSize + wallSize * 2;
+  const sidebarSize = 192;
+  return {
+    floorHeight,
+    wallSize,
+    gameAreaSize,
+    canvasSize,
+    sidebarSize,
+    sceneWidth: canvasSize + sidebarSize * +!isMobile,
+    sceneHeight: canvasSize + sidebarSize * +isMobile,
+  };
+};
+
 export class GameScene extends BaseScene {
+  sceneDimensions: GameSceneDimensions;
   constructor(game: IGame) {
     super();
-    game.resize(600 + 200 * +!isMobile, 600 + 200 * +isMobile);
+    const sceneDimensions = getGameSceneDimensions(8);
+    game.resize(sceneDimensions.sceneWidth, sceneDimensions.sceneHeight);
+    this.sceneDimensions = sceneDimensions;
   }
-  update(dt: number): void {}
+  // update(dt: number): void {}
   draw(context: CanvasRenderingContext2D): void {
-    context.fillStyle = "green";
+    context.fillStyle = COLOR_WHITE;
     context.fillRect(0, 0, c.width, c.height);
   }
   onClick(mouseX: number, mouseY: number): void {
--- src/scenes/title-scene.ts
+++ src/scenes/title-scene.ts
@@ -1,24 +1,27 @@
 import { sine } from "../core/easing";
 import { IGame } from "../game";
 import { COLOR_BLACK, COLOR_WHITE, isMobile } from "../registry";
+import { getGameSceneDimensions } from "./game-scene";
 import { BaseScene, SceneName } from "./scene";
+
+const titleText = "XIII FLOOR";
 
 export class TitleScene extends BaseScene {
   private doorWidth: number;
-  private t = 0;
+  private frameCounter = 0;
 
   constructor(game: IGame) {
     super();
 
-    // TODO: get game size from the GameScene
+    const { sceneWidth, sceneHeight } = getGameSceneDimensions(8);
     const scaleMult = 2;
-    game.resize((600 + 200 * +!isMobile) * scaleMult, (600 + 200 * +isMobile) * scaleMult);
+    game.resize(sceneWidth * scaleMult, sceneHeight * scaleMult);
 
     this.doorWidth = c.width / 2;
 
     onclick = () => {
       onclick = null;
-
+      // TODO: tune tween
       game.tweener.tweenProperty(
         30,
         this.doorWidth,
@@ -32,29 +35,30 @@ export class TitleScene extends BaseScene {
     };
   }
   update(dt: number): void {
-    this.t++;
-    this.t %= 400;
+    this.frameCounter = (this.frameCounter + 1) % 1000;
   }
   draw(context: CanvasRenderingContext2D): void {
+    const { doorWidth, frameCounter } = this;
+    const { width, height } = c;
+
     context.fillStyle = COLOR_WHITE;
-    context.fillRect(0, 0, c.width, c.height);
+    context.fillRect(0, 0, width, height);
 
     context.fillStyle = COLOR_BLACK;
-    context.fillRect(0, 0, this.doorWidth, c.height);
-    context.fillRect(c.width / 2 + (c.width / 2 - this.doorWidth), 0, this.doorWidth, c.height);
+    context.fillRect(0, 0, doorWidth, height);
+    context.fillRect(width - doorWidth, 0, doorWidth, height);
 
-    const text = "XIII FLOOR";
-    context.font = "bold 1px Georgia";
-    const fontSize = (c.width * 0.9) / context.measureText(text).width;
-    context.font = `bold ${fontSize}px Georgia`;
     context.fillStyle = COLOR_WHITE;
-    context.textAlign = "center";
-    context.fillText(text, c.width / 2, c.height / 2);
 
-    if (Math.floor(this.t / 40) % 2 === 0) return;
+    context.font = "bold 1px Georgia";
+    context.font = `bold ${(width * 0.9) / context.measureText(titleText).width}px Georgia`;
+    context.textAlign = "center";
+    context.fillText(titleText, width / 2, height / 2);
+
+    if (~~(frameCounter / 100) % 2 === 0) return;
 
     context.font = "32px Arial";
-    context.fillText("click to start", c.width / 2, c.height * 0.6);
+    context.fillText(`${isMobile ? "tap" : "click"} to start`, width / 2, height * 0.6);
   }
   destroy(): void {
     onclick = null;
