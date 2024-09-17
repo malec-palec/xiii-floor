5c56f144de936c3685d76bb308449c338dbd728c
glebv <glebusheg@gmail.com>
2024-09-18 01:18:50 +0200

    optimize build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,35 @@
+import { advzipPlugin, defaultAdvzipOptions, defaultEctOptions, ectPlugin } from "js13k-vite-plugins";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   base: "./",
-  plugins: [],
+  plugins: [
+    viteSingleFile({ useRecommendedBuildConfig: false, removeViteModuleLoader: true }),
+    createHtmlPlugin({ minify: true }),
+    ectPlugin(defaultEctOptions),
+    advzipPlugin(defaultAdvzipOptions),
+    {
+      name: "final-transformations",
+      enforce: "post",
+      renderChunk: async (code: string) => {
+        return {
+          // @ts-expect-error trust me - it's ES2021
+          code: code.replaceAll("const ", "let "),
+          map: null,
+        };
+      },
+    },
+  ],
   build: {
+    assetsDir: ".",
     assetsInlineLimit: 0,
-  },
-  preview: {
-    open: true,
-  },
-  esbuild: {
-    legalComments: "external",
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]", // Keep original asset names
+      },
+    },
   },
 });
